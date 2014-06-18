3cc6bced3ed760b769d836c251d3af3726a5e7d6
840_ <nyarlatlohotep@gmail.com>
2014-06-19 01:15:14 +0900

    演習３

--- hello.rb
+++ hello.rb
@@ -1,5 +1,11 @@
-msg = "Hello World"
-p msg
+profile = [{:names => "静岡太郎",:age => "34",:loc => "静岡"},{:names => "名古屋二郎",:age => "25",:loc => "愛知"},{:names => "大津三郎",:age=>"19",:loc=>"岐阜"}]
 
-MSG = "helll"
-p MSG.length()
+def search(profile,key,query)
+	profile.each do |data|
+		if query =~ data[key]
+			return data
+		end
+	end
+end
+
+p search(profile,:loc,/静岡/)
